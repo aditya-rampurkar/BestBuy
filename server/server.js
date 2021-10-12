d33d71f6fd411472568104c9e4252be85a0759b6
vikassharma1999 <vikas.aipro@gmail.com>
2021-10-12 20:31:23 +0530

    payment option removed from server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
 const cartRoutes = require('./routes/cartRoutes')
 const orderRoutes = require('./routes/orderRoutes')
-const paymentRoutes = require('./routes/paymentRoutes')
+// const paymentRoutes = require('./routes/paymentRoutes')
 
 const mongoUrl = "mongodb+srv://vikas:vikas123@apnidukan.d2e3h.mongodb.net/apniDukan?retryWrites=true&w=majority"
 
@@ -30,6 +30,6 @@ app.use("/auth",userRoutes)
 app.use("/product",productRoutes)
 app.use("/cart",cartRoutes)
 app.use("/order",orderRoutes)
-app.use("/payment",paymentRoutes)
+// app.use("/payment",paymentRoutes)
 
 app.listen(port,(req,res)=>console.log(`Server is running at ${port}`))
